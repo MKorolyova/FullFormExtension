ff61d83489036a35abcabbbabe935bffb9e32a0a
MKorolyova <mkorolyova52@gmail.com>
2025-10-26 07:35:21 -0500

    start script corrections

--- start.sh
+++ start.sh
@@ -1,3 +1,12 @@
-    #!/bin/sh
-    chmod +x ./sv.py
-    ./sv.py
+#!/bin/bash
+
+[ ! -d "venv" ] && python3 -m venv venv
+source venv/bin/activate
+
+pip install --upgrade pip > /dev/null 2>&1
+pip install fastapi uvicorn google-generativeai > /dev/null 2>&1
+
+chmod +x ./sv.py
+
+./sv.py
+
