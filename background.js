de123e5661d4efdc47987ff7a0719ad7cfb77c42
MKorolyova <mkorolyova52@gmail.com>
2025-10-25 21:10:55 -0500

    Gemini request test extension

--- background.js
+++ background.js
@@ -4,10 +4,11 @@ import { GoogleGenAI } from "@google/genai";
 // The client gets the API key from the environment variable `GEMINI_API_KEY`.
 const ai = new GoogleGenAI({});
 
-async function askAI() {
+async function askAI(language) {
   const response = await ai.models.generateContent({
     model: "gemini-2.5-flash",
-    contents: "Explain how AI works in a few words",
+    contents: "translate the following text to " + language +  text,
   });
   return response.text;
 }
+
--- popup.js
+++ popup.js
@@ -1,7 +1,25 @@
+import { GoogleGenAI } from "@google/genai";
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
   const input = document.getElementById('inputText');
-  const btn = document.getElementById('expandBtn');
+  const btn = document.getElementById('translateBtn');
   const output = document.getElementById('output');
+  const language = document.getElementById('languageSelect');
+
+
+    // The client gets the API key from the environment variable `GEMINI_API_KEY`.
+    const ai = new GoogleGenAI({});
+
+async function askAI(language) {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: "translate the following text to " + language +  text,
+  });
+  return response.text;
+}
+
 
   function showMessage(msg){
     output.textContent = msg;
@@ -10,9 +28,9 @@ document.addEventListener('DOMContentLoaded', () => {
   btn.addEventListener('click', () => {
     const text = (input.value || '').trim();
     if (!text) {
-        translation = background.askAI();
+        translation = background.askAI(language.value, text);
     }
-    showMessage( translation);
+    showMessage(translation);
   });
 
 
@@ -23,3 +41,4 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
+
